dbba50d820f48f674c3c7014840ef51fbab5eaac
Ronald <sincheleivaronald@gmail.com>
2022-07-12 11:07:30 -0500

    Initial commit

---
+++ src/main/java/com/ronald/project/service/CustomerServiceImp.java
@@ -0,0 +1,37 @@
+package com.ronald.project.service;
+
+import com.ronald.project.model.document.Customer;
+import com.ronald.project.model.repository.CustomerRepository;
+import com.ronald.project.model.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CustomerServiceImp implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Flux<Customer> getCustomer() {
+        return this.customerRepository.findAll();
+    }
+
+    @Override
+    public Mono<Customer> saveCustomer(Customer customer) {
+        return this.customerRepository.save(customer);
+    }
+
+    @Override
+    public Mono<Customer> findCustomer(String id) {
+        System.out.println("Cacheable data");
+        return this.customerRepository.findById(id);
+    }
+
+    @Override
+    public Mono<Void> deleteCustomer(String id) {
+        System.out.println("Cache evict");
+        return this.customerRepository.deleteById(id);
+    }
+}
